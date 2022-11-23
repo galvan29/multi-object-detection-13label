5b6bd7ec7c5eac638a76b493db36a81b70e654bb
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-11-23 18:01:26 +0100

    src: TO FIX, load dataset

---
+++ src/multiobjdetect.py
@@ -0,0 +1,26 @@
+# Il nostro codice
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import pickle as pk
+from torchvision.datasets import ImageFolder
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+# Caricamento dei dati
+if not os.path.exists("assignment_1"):
+    ! unzip -P dluniud2022 /content/drive/Shareddrives/Razor/Root/4Anno/DeepLearning/datasets/cldataset.zip 
+
+#train and test data directory
+data_dir = "/content/assignment_1/train/images"
+test_data_dir = "/content/assignment_1/test/images"
+
+#load the train and test data
+dataset = ImageFolder()
