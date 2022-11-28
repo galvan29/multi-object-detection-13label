9a38810da95e28aa3edca6872e15cd65efe7815b
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-11-28 17:21:33 +0100

    src: dividing into functions

--- src/devideclasses.py
+++ src/devideclasses.py
@@ -33,11 +33,10 @@ def preprocess_dataset():
     # Lists that will contain the whole dataset
     labels = []
     boxes = []
+    images = []
+    scale = 256
 
-    h = 256
-    w = 256
-
-    rows = os.listdir(image_dir + "images")[:21]
+    rows = os.listdir(image_dir + "images")[::]
     cicle = 0
     for row in rows[:21]:
         stringa = str(row[0:6]) + ".json"
@@ -45,57 +44,56 @@ def preprocess_dataset():
             data = json.loads(f.read())
             array = items(data)
             
-            #img_path = row
-            #img  = cv2.imread(os.path.join(image_dir + "images/",img_path))
-            #image = cv2.resize(img, (64, 64))
-            #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            #image = image.astype("float") / 255.0
+            img_path = row
+            image = cv2.imread(os.path.join(image_dir + "images/",img_path))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = image.astype("float") / (255.0) 
 
-            
             boxes.append([])
             labels.append([])
-
+            images.append(image)
             for item in array:
                 boxes[cicle].append(item['bounding_box'])
                 labels[cicle].append(item['category_name'])
 
             cicle += 1
 
-    return labels, boxes, rows 
+    return labels, boxes, images 
 
+def display_boxes(labels, boxex, img_list):
+    combined_list = list(zip(img_list, boxes, labels))
+    # Generate a random sample of images each time the cell is run 
+    random_range = random.sample(range(1, len(img_list)), 20)
+
+    for itr, i in enumerate(random_range, 1):
+        img_size = 256 
+        image  = img_list[i] 
+
+        # Draw bounding boxes on the image
+        for box in boxes[i]:
+            a1, b1, a2, b2 = box
+            x1 = a1
+            x2 = a2
+            y1 = b1
+            y2 = b2
+            print(a1, x1, b1, y1)
+            print(a2, x2, b2, y2)
+            print()
+            cv2.rectangle(image, (int(x1),int(y1)),
+                (int(x2),int(y2)),
+                        (255,0,0),
+                        3)
+    
+        # Clip the values to 0-1 and draw the sample of images
+        #img = np.clip(image, 0, 1)
+        plt.imshow(image)
+        plt.axis('off')
+    plt.show()
+
+# Load images, labels, boxes
 labels, boxes, img_list = preprocess_dataset()
-combined_list = list(zip(img_list, boxes, labels))
+
+display_boxes(labels, boxes, img_list)
 
 
-# Create a Matplotlib figure
-#plt.figure(figsize=(20,20))
 
-# Generate a random sample of images each time the cell is run 
-random_range = random.sample(range(1, len(img_list)), 20)
-
-for itr, i in enumerate(random_range, 1):
-    # Bounding box of each image
-    
-    img_size = 256
-    ## Rescaling the boundig box values to match the image size
-    
-    # The image to visualize
-    img  = cv2.imread(os.path.join(image_dir + "images/", img_list[i]))
-    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    image = image.astype("float") / (img_size-1)
-
-    # Draw bounding boxes on the image
-    for box in boxes[i]:
-        a1, b1, a2, b2 = box
-        cv2.rectangle(image, (int(a1),int(b1)),
-            (int(a2),int(b2)),
-                    (255,0,0),
-                    3)
-    
-    # Clip the values to 0-1 and draw the sample of images
-    img = np.clip(image, 0, 1)
-    plt.subplot(4, 5, itr)
-    plt.imshow(img)
-    plt.axis('off')
-
-plt.show()
